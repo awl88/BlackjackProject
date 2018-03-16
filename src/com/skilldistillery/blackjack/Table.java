24fef868a4196aff23551dd41e3fa202128d9051
Andrew Lyon <anlyon99@gmail.com>
2018-03-16 17:29:30 -0600

    mvp

--- src/com/skilldistillery/blackjack/Table.java
+++ src/com/skilldistillery/blackjack/Table.java
@@ -17,6 +17,7 @@ public class Table {
 
 	public static void main(String[] args) {
 		Table deal = new Table();
+		System.out.println("\u2660\u2663\u2665\2666");
 		deal.start(kb);
 		kb.close();
 	}
@@ -46,6 +47,7 @@ public class Table {
 				game();
 			} else if (playAgain.equalsIgnoreCase("N")) {
 				System.out.println("Thank you for playing, have a great day.");
+				break;
 			} else {
 				System.out.println("Please input Y or N");
 			}
@@ -55,7 +57,7 @@ public class Table {
 
 	public void game() {
 		// Dealer deals cards
-		int x = 0;
+		int faceUp = 0;
 		for (int i = 0; i < 2; i++) {
 			Card gamblerCard = d1.dealCards();
 			p1.addCard(gamblerCard);
@@ -63,43 +65,73 @@ public class Table {
 			Card dealerCard = d1.dealCards();
 			d1.addCard(dealerCard);
 
-			if (x == 0) {
+			if (faceUp == 0) {
 				System.out.println("The dealer deals himself a card facedown.");
-				x++;
+				faceUp++;
 			} else {
 				System.out.println("The dealer deals himself a " + dealerCard);
 			}
 
 		}
 		// Hit or stay?
+		int keepRunning = 0;
 		boolean keepGoing = true;
 		System.out.println();
-		System.out.println("==It is now your turn==");
+		System.out.println(
+				"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665");
+		System.out.println("\u2660\u2663It is now your turn\u2665\u2666");
+		System.out.println(
+				"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665");
 		System.out.println();
 		while (keepGoing) {
 			while (true) {
 				System.out.println("The cards in your hand right now are " + p1.getHand());
 				System.out.println("Your hand's value is currently " + p1.getCardValue());
-				System.out.println("==================");
+				System.out.println();
+				System.out.println(
+						"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660");
+				System.out.println();
 				if (p1.getCardValue() > 21) {
+					System.out.println(
+							"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665");
 					System.out.println("You bust!");
+					System.out.println(
+							"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665");
 					youLose();
+					keepRunning = 1;
 					keepGoing = false;
 					break;
 				}
 				System.out.println("Would you like to:");
 				System.out.println("1. Hit");
 				System.out.println("2. Stay");
-				int hitOrStay = kb.nextInt();
-				if (hitOrStay == 1) {
-					Card hitMe = d1.dealCards();
-					p1.addCard(hitMe);
-				} else {
-					break;
+				while (true) {
+					int hitOrStay = 0;
+					try {
+						hitOrStay = kb.nextInt();
+					} catch (InputMismatchException e) {
+						System.out.println("Please input 1 to hit, or 2 to stay");
+					}
+					if (hitOrStay == 1) {
+						Card hitMe = d1.dealCards();
+						p1.addCard(hitMe);
+					} else if (hitOrStay == 2) {
+						break;
+					} else {
+						continue;
+					}
 				}
+				break;
+			}
+			if (keepRunning == 1) {
+				break;
 			}
 			System.out.println();
-			System.out.println("==It is now the Dealer's turn==");
+			System.out.println(
+					"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666");
+			System.out.println("\u2660\u2663It is now the Dealer's turn\u2665\u2666");
+			System.out.println(
+					"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666");
 			System.out.println();
 			System.out.println("The dealer shows his hand, he currently has " + d1.getHand());
 			System.out.println("The value of the dealer's hand is currently " + d1.getCardValue());
@@ -115,6 +147,7 @@ public class Table {
 				System.out.println("Dealer busts!");
 				youWin();
 				keepGoing = false;
+				break;
 			}
 			// Decide winners
 			if (p1.getCardValue() == d1.getCardValue()) {
@@ -129,18 +162,24 @@ public class Table {
 
 	public void youWin() {
 		System.out.println();
-		System.out.println("===================");
+		System.out.println(
+				"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665");
 		System.out.println("You win this round!");
-		System.out.println("===================");
+		System.out.println(
+				"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665");
 		System.out.println();
+
 	}
 
 	public void youLose() {
 		System.out.println();
-		System.out.println("===================");
+		System.out.println(
+				"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665");
 		System.out.println("You lose this round. Better luck next time!");
-		System.out.println("===================");
+		System.out.println(
+				"\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665\u2666\u2660\u2663\u2665");
 		System.out.println();
+
 	}
 
 }
