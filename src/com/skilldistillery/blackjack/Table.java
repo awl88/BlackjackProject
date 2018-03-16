0f9c85371ff392109ce1b131e5bc224429984a7f
Andrew Lyon <anlyon99@gmail.com>
2018-03-16 17:00:14 -0600

    almost mvp, need to fix a few bugs

--- src/com/skilldistillery/blackjack/Table.java
+++ src/com/skilldistillery/blackjack/Table.java
@@ -1,32 +1,59 @@
 package com.skilldistillery.blackjack;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.skilldistillery.cards.common.Card;
 
 public class Table {
+	static Scanner kb = new Scanner(System.in);
+	// Create player
+	Hand p1Hand = new Hand();
+	Player p1 = new Gambler("", p1Hand);
+
+	// Create Dealer
+	Hand d1Hand = new Hand();
+	Dealer d1 = new Dealer("Dealer", d1Hand);
 
 	public static void main(String[] args) {
 		Table deal = new Table();
-		Scanner kb = new Scanner(System.in);
 		deal.start(kb);
 		kb.close();
 	}
 
 	public void start(Scanner kb) {
-		// Start game & create Player
+		// Start game
 		System.out.print("Welcome to the Blackjack table. What is your name? ");
 		String name = kb.next();
-		Hand p1Hand = new Hand();
-		Player p1 = new Gambler(name, p1Hand);
 
-		// Create Dealer
-		Hand d1Hand = new Hand();
-		Dealer d1 = new Dealer("Dealer", d1Hand);
+		p1.setName(name);
 
 		// Dealer shuffles deck
 		d1.shuffleCards();
 
+		// Call game method
+		while (true) {
+			game();
+
+			System.out.print("Would you like to play another round? (Y/N) ");
+			String playAgain = "N";
+			try {
+				playAgain = kb.next();
+			} catch (InputMismatchException e) {
+				System.out.println("Please input Y or N");
+			}
+			if (playAgain.equalsIgnoreCase("Y")) {
+				game();
+			} else if (playAgain.equalsIgnoreCase("N")) {
+				System.out.println("Thank you for playing, have a great day.");
+			} else {
+				System.out.println("Please input Y or N");
+			}
+		}
+
+	}
+
+	public void game() {
 		// Dealer deals cards
 		int x = 0;
 		for (int i = 0; i < 2; i++) {
@@ -34,6 +61,8 @@ public class Table {
 			p1.addCard(gamblerCard);
 			System.out.print("You were dealt a : " + gamblerCard + "  \t");
 			Card dealerCard = d1.dealCards();
+			d1.addCard(dealerCard);
+
 			if (x == 0) {
 				System.out.println("The dealer deals himself a card facedown.");
 				x++;
@@ -42,44 +71,76 @@ public class Table {
 			}
 
 		}
-		while (true) {
-			System.out.println("The cards in your hand right now are " + p1.getHand());
-			System.out.println("Your hand's value is currently " + p1.getCardValue());
-			System.out.println("==================");
-			System.out.println("Would you like to:");
-			System.out.println("1. Hit");
-			System.out.println("2. Stay");
-			int hitOrStay = kb.nextInt();
-			if (hitOrStay == 1) {
-				Card nextCard = d1.dealCards();
-				p1.addCard(nextCard);
-			} else {
-				break;
+		// Hit or stay?
+		boolean keepGoing = true;
+		System.out.println();
+		System.out.println("==It is now your turn==");
+		System.out.println();
+		while (keepGoing) {
+			while (true) {
+				System.out.println("The cards in your hand right now are " + p1.getHand());
+				System.out.println("Your hand's value is currently " + p1.getCardValue());
+				System.out.println("==================");
+				if (p1.getCardValue() > 21) {
+					System.out.println("You bust!");
+					youLose();
+					keepGoing = false;
+					break;
+				}
+				System.out.println("Would you like to:");
+				System.out.println("1. Hit");
+				System.out.println("2. Stay");
+				int hitOrStay = kb.nextInt();
+				if (hitOrStay == 1) {
+					Card hitMe = d1.dealCards();
+					p1.addCard(hitMe);
+				} else {
+					break;
+				}
+			}
+			System.out.println();
+			System.out.println("==It is now the Dealer's turn==");
+			System.out.println();
+			System.out.println("The dealer shows his hand, he currently has " + d1.getHand());
+			System.out.println("The value of the dealer's hand is currently " + d1.getCardValue());
+			// Dealer checks his hand, if < 17, dealer hits
+			while (d1.getCardValue() < 17) {
+				Card hitMe = d1.dealCards();
+				System.out.println("The dealer drew a " + hitMe);
+				d1.addCard(hitMe);
+				System.out.println("The dealer's hand is currently " + d1.getHand());
+				System.out.println("The value of the dealer's hand is currently " + d1.getCardValue());
+			}
+			if (d1.getCardValue() > 21) {
+				System.out.println("Dealer busts!");
+				youWin();
+				keepGoing = false;
+			}
+			// Decide winners
+			if (p1.getCardValue() == d1.getCardValue()) {
+				System.out.println("It's a tie!");
+			} else if (p1.getCardValue() > d1.getCardValue()) {
+				youWin();
+			} else if (p1.getCardValue() < d1.getCardValue()) {
+				youLose();
 			}
 		}
+	}
 
-		// Decide winners
+	public void youWin() {
+		System.out.println();
+		System.out.println("===================");
+		System.out.println("You win this round!");
+		System.out.println("===================");
+		System.out.println();
+	}
 
-		// deck.shuffle();
-		// System.out.print("How many cards would you like? ");
-		// int dealAmount = 0;
-		// try {
-		// dealAmount = kb.nextInt();
-		// } catch (InputMismatchException e) {
-		// System.out.println("No, we need a number.");
-		// }
-		//
-		// if (dealAmount > 52) {
-		// System.out.println("We only have " + deck.checkDeckSize() + " cards in the
-		// deck...");
-		// return;
-		// }
-		// for (int i = 0; i < dealAmount; i++) {
-		// Card card = deck.dealCard();
-		// System.out.println(card.toString());
-		// }
-		// System.out.println("There are " + deck.checkDeckSize() + " cards left in the
-		// deck.");
+	public void youLose() {
+		System.out.println();
+		System.out.println("===================");
+		System.out.println("You lose this round. Better luck next time!");
+		System.out.println("===================");
+		System.out.println();
 	}
 
 }
