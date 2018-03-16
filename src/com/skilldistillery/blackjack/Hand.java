58c48e2dc247e83f6f5a3379078168fff6770b0d
Andrew Lyon <anlyon99@gmail.com>
2018-03-16 14:22:48 -0600

    Stub out README.md

--- src/com/skilldistillery/blackjack/Hand.java
+++ src/com/skilldistillery/blackjack/Hand.java
@@ -1,5 +1,22 @@
 package com.skilldistillery.blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hand {
 	private List<Card> hand = new ArrayList<>();
+
+	public void addCard() {
+
+	}
+
+	public Hand getCardsInHand() {
+
+		return this;
+	}
+
+	public int getValueOfHand() {
+
+		return -1;
+	}
 }
---
+++ src/com/skilldistillery/blackjack/Player.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.blackjack;
+
+public abstract class Player {
+	private String name;
+	private Hand hand;
+	
+	
+
+}
